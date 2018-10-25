e0aa881d9f7c882e78ab50448aedc9f70cbe407d
PRASHANT-2018-KUSHWAHA <prashantkushwaha000@gmail.com>
2018-10-25 10:36:52 +0530

    fixing angular

---
+++ angular/src/app/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private _http: HttpClient) { }
+
+
+  register(body: any) {
+      return this._http.post('http://127.0.0.1:3000/users/register', body, {
+        observe: 'body', // so that httpclient class look for body  not the complete response
+        headers: new HttpHeaders().append('Content-Type', 'application/json')
+      });
+  }
+
+  login(body: any) {
+    return this._http.post('http://127.0.0.1:3000/users/login', body, {
+      observe: 'body', // so that httpclient class look for body  not the complete response
+      withCredentials: true,  // without this cooki will not sent to the browser because in app.js file in cors we define credentials: true
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
+
+  home() {
+    return this._http.get('http://127.0.0.1:3000/users/home', {
+      observe: 'body', // so that httpclient class look for body  not the complete response
+      withCredentials: true,  // without this cooki will not sent to the browser because in app.js file in cors we define credentials: true
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
+
+  logout() {
+    return this._http.get('http://127.0.0.1:3000/users/logout', {
+      observe: 'body', // so that httpclient class look for body  not the complete response
+      withCredentials: true,  // without this cooki will not sent to the browser because in app.js file in cors we define credentials: true
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
+}
